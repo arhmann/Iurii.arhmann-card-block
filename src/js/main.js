561faafea160b0311bdfce58e7b5d8a04eb9867f
iurii.a <iu.arhmann@gmail.com>
2020-10-22 20:00:52 +0300

    fix ajax

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,52 @@
 "use strict";
 //const axios = require("axios");
 let buttons = document.querySelectorAll(".btn--buy");
-//const servResponse = document.querySelector(".response");
 let xhr = new XMLHttpRequest();
+let arrayBTN = [];
+/*
+window.onload = () => {
+    buttons.forEach((btn) => {
+      if (localStorage.getItem() == localStorage.getItem("userID")) {
+        btn.textContent = "В корзине.";
+        btn.style.display = "inline";
+        console.log(localValue)
+          JSON.stringify(arrayBTN)
+      }
+    });
+};
+*/
 
-const getAJAX = () => {
+const setAJAX = () => {
   xhr.open("GET", "https://reqres.in/api/products/3", true);
   xhr.send();
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      sendXHR(this.responseText);
+      this.responseText;
     }
   };
 };
 
+buttons.forEach((btn) =>
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    localStorage.setItem("userID", "337848071");
 
-buttons.forEach(el => 
-    el.addEventListener("click", (event) => {
-        event.preventDefault();
-        el.classList.toggle('loading');
+    btn.classList.toggle("loading");
+    
+    arrayBTN.push(btn.dataset.dataid);
+    console.log(arrayBTN);
 
-         setTimeout(function(){
-          if (xhr.readyState === 4 && xhr.status === 200) {
-            el.classList.toggle('loading');
-            el.textContent = "В корзине."
-            
-          }
-         },1800);
+    setTimeout(function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        btn.classList.toggle("loading");
+        btn.textContent = "В корзине.";
         
-        getAJAX();
-      })
-    );
- 
-function sendXHR(data) {
-  //just for test!
-  console.log(data);
-}
+      }
+    }, 1000);
+    setAJAX();
+    
+    
+  })
+);
+
 
