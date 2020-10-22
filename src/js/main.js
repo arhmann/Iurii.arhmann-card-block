0463af1b3c1a45b093b11ad81861e11df4ff61d9
iurii.a <iu.arhmann@gmail.com>
2020-10-22 16:10:13 +0300

    added animation and ajax send

--- src/js/main.js
+++ src/js/main.js
@@ -14,16 +14,26 @@ const getAJAX = () => {
   };
 };
 
+
 buttons.forEach(el => 
     el.addEventListener("click", (event) => {
         event.preventDefault();
+        el.classList.toggle('loading');
+
+         setTimeout(function(){
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            el.classList.toggle('loading');
+            el.textContent = "В корзине."
+            
+          }
+         },1800);
+        
         getAJAX();
       })
     );
-  
-
-
+ 
 function sendXHR(data) {
   //just for test!
   console.log(data);
 }
+
